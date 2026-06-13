d1d825a03085fe331bf8ee463ee6ea2ee1145736
Solothiel <dste486@wgu.edu>
2026-06-13 15:00:36 -0600

    rebuilt renamer_core.py

--- renamer_core.py
+++ renamer_core.py
@@ -1,13 +1,20 @@
 import os
 import re
 
+STOPWORDS = {"copy", "final", "new", "v1", "v2", "draft"}
+
 def generate_suggestions(filename):
     name, _ = os.path.splitext(filename)
 
     name = name.replace("_", " ")
     name = name.replace("-", " ")
+
     name = re.sub(r"\(\d+\)", "", name)
+
+    words = name.split()
+    words = [w for w in words if w.lower() not in STOPWORDS]
+
+    name = " ".join(words)
     name = re.sub(r"\s+", " ", name).strip()
 
     return name.title()
-
